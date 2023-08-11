aeaf868390babd6d6cd08f007e22e78967c4eb9d
Nathan Bunker <nbunker@immregistries.org>
2023-08-11 13:18:45 -0600

    Added python script - made files pretty

---
+++ pretty-html.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import os
+import logging
+
+# Configure logging
+logging.basicConfig(filename='error_log.log', level=logging.ERROR)
+
+def prettify_html(dir_path):
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.html'):
+                try:
+                    file_path = os.path.join(root, file)
+                    with open(file_path, 'r') as f:
+                        soup = BeautifulSoup(f, 'html.parser')
+
+                    pretty_html = soup.prettify()
+
+                    with open(file_path, 'w') as f:
+                        f.write(pretty_html)
+                    print(f"Reformatted file {file_path}")
+                except Exception as e:
+                    logging.error(f"Error reformatting file {file_path}: {e}")
+
+current_directory = os.getcwd()
+print('Will prettify data from this directory: ' + current_directory)
+
+prettify_html(current_directory)
