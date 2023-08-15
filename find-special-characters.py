26f7990dab77aa803ad340f0bf378c7e51eeb819
Nathan Bunker <nbunker@immregistries.org>
2023-08-15 13:37:19 -0400

    Now checking for errors in files

--- find-special-characters.py
+++ find-special-characters.py
@@ -7,7 +7,7 @@ def find_special_characters(directory):
             if file_name.endswith(".html"):
                 print(f' - Looking at file: {file_name}')
                 file_path = os.path.join(root, file_name)
-                with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+                with open(file_path, 'r', encoding='utf-8') as file:
                     content = file.read()
                     if re.search(r'[^\x00-\x7F]', content):
                         print(f" + File with special characters found: {file_path}")
