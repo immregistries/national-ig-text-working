5f7e97ac570ab0426ac678a1f33e02aff340c54b
Nathan Bunker <nbunker@immregistries.org>
2023-09-18 08:43:57 -0600

    Made sure directories are added even if empty

--- zipup.py
+++ zipup.py
@@ -3,7 +3,12 @@ import os
 
 def zip_html_files(zipf_path):
     with zipfile.ZipFile(zipf_path, 'w') as zipf:
-        for root, _, files in os.walk('.'):
+        for root, dirs, files in os.walk('.'):
+            # Add directories even if empty
+            for dir in dirs:
+                dir_path = os.path.join(root, dir)
+                zipf.write(dir_path, os.path.relpath(dir_path))
+            # Add HTML files
             for file in files:
                 if file.endswith('.html'):
                     path_in_zip = os.path.join('/', os.path.relpath(root), file)
